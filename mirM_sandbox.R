87dd76756a0693e65e8134eab597015486fd8497
Christiaan Klijn <c.klijn@gmail.com>
2011-11-11 16:38:50 +0100

    Added to the sandbox code, started SAM code

--- mirM_sandbox.R
+++ mirM_sandbox.R
@@ -5,10 +5,35 @@
 # Description: Sandbox code
 # -------------------------------------------------------------------
 
+# To Do
+# Use SAM to find significant miRs
+# Find optimal delta
+# define comparison groups
+# eliminate non-variant mirs
+
 # Change these to the local directories
 # Directories shown are mediod dirs
 dataDir <- '~/data/smallproj/martineMir/'
 codeDir <- '~/gitCodeChris/MartineMirs/'
-
 setwd(dataDir)
+
+# Source functions
+source(paste(codeDir, 'mirM_functions.R', sep=''))
+# Use the siggenes library for the sam analysis
+library(siggenes)
+
+# Functions
+
+# Load data
+load('mirM_rawData.Rda')
+
+# do a sample sam analysis
+# Check all lung mets vs. their donor tumors
+
+arrayLung <- subset(hybInfo, sampleType == 'Lung met')$Array
+arrayNonLung <- subset(hybInfo, sampleType != 'Lung met')$Array
+
+lungSam <- sam(measureData, colnames(measureData) %in% arrayLung)
+lungSamSummary <- summary(lungSam, delta=1.2)
+
 

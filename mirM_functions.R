fb04f27e2754ea5bebb6ba17cdfbde450b4ebb4b
Christiaan Klijn <c.klijn@gmail.com>
2011-11-11 14:18:59 +0100

    Added function file

---
+++ mirM_functions.R
@@ -0,0 +1,7 @@
+# mirM_functions.R
+# -------------------------------------------------------------------
+# Copyright 2011 Christiaan Klijn <c.klijn@nki.nl>
+# Project: microRNA analyses Martine
+# Description: Functions for project
+# -------------------------------------------------------------------
+
